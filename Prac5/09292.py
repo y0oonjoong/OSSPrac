aca39bbc234ec28d65c60de0aa37b3090701c6c5
YoonJ <zezegpffh@dgu.ac.kr>
2024-10-01 16:07:53 +0900

    add grade

--- Prac5/09292.py
+++ Prac5/09292.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요 :  ")
 number = input("학번을 입력하세요 :  ")
-class1 = input("학과을 입력하세요 :  ")
+class1 = input("학과를 입력하세요 :  ")
+grade = input("학년을 입력하세요 :  ")
 
 print("<출력>")
 print(f"이름: {name}")
 print(f"학번: {number}")
 print(f"학과: {class1}")
+print(f"학과: {grade}")
